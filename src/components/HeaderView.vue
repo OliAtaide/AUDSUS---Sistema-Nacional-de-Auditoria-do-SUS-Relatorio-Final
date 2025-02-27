853cd44a285a84a3646134fd9fe852e84fef70d5
Filipe de Oliveira de Ataíde <filipe.ataide@lais.huol.ufrn.br>
2025-02-27 10:20:41 -0300

    header, footer e sass

---
+++ src/components/HeaderView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="navbar header">
+    <div class="container">
+      <a href="" class="navbar-brand">
+        <img src="@/assets/logo.svg" alt="" />
+      </a>
+      <a class="ext" href="" target="_blank">
+        Portal LAIS
+        <i class="bi bi-box-arrow-up-right"></i>
+      </a>
+    </div>
+  </div>
+  <div class="navbar subheader py-3">
+    <div class="container mb-3">
+      <div class="row w-100">
+        <div class="col">
+          <span>
+            <strong> Módulo: </strong>
+            Sistema Nacional de Auditoria do SUS: instrumento de governança
+            pública e de fortalecimento do controle social.
+          </span>
+        </div>
+        <div class="col">
+          <span>
+            <strong> Unidade 3: </strong>
+            Conhecendo os principais aspectos do relatório de auditoria
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row w-100">
+        <div class="col">
+          <span>
+            <strong> Aula 1: </strong>
+            Relatório Preliminar de auditoria
+          </span>
+        </div>
+        <div class="col">
+          <span>
+            <strong> Conteudista: </strong>
+            Márcia Andrade de Souza
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
